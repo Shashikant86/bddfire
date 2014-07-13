cea202fa3ae38dadb29e2daf6592bb139881f323
Shashikant86 <shashikant.jagtap@aol.co.uk>
2014-07-13 21:26:05 +0100

    Added support for the Sauce and Appium

--- scaffold/features/support/env.rb
+++ scaffold/features/support/env.rb
@@ -41,3 +41,30 @@ Capybara.register_driver :browserstack do |app|
       :url => "http://USERNAME:API_KEY@hub.browserstack.com/wd/hub"
     )
 end
+
+Capybara.register_driver :sauce do |app|
+  job_name = "Cucumber-Tests - #{Time.now.strftime '%Y-%m-%d %H:%M'}"
+  browser =  ENV['SAUCE_BROWSER']  || 'firefox'
+  version =  ENV['SAUCE_VERSION']  || '3.6'
+  platform = ENV['SAUCE_PLATFORM'] || 'WINDOWS'
+  duration = 7200
+  capabilities = {:browserName => browser, :version => version, :platform => platform, :name => job_name, "max-duration" => duration}
+  puts "Running #{job_name} on SauceLabs with #{browser} #{version} on #{platform}"
+  Capybara::Selenium::Driver.new(app,
+    :browser => :remote,
+    :desired_capabilities => capabilities,
+    :url => "http://SAUCE_USERNAME:SAUCE_API_KEY@ondemand.saucelabs.com:80/wd/hub")
+end
+
+Capybara.register_driver :appium do |app|
+    browserName = ENV['APPIUM_BROWSER_NAME'] || 'iOS'
+    device = ENV['APPIUM_DEVICE'] || 'iPhone Simulator'
+    platform = ENV['APPIUM_OS'] || 'Mac'
+    os_version = ENV['APPIUM_OS'] || '6.1'
+    app = ENV['APPIUM_OS'] || 'safari'
+    capabilities = {:browserName => browserName,  :os_version => os_version, :device => device,  "platform" => platform, "app"=>app}
+Capybara::Selenium::Driver.new(app,
+                                 :browser => :remote,
+                                 :desired_capabilities => capabilities,
+                                 :url => "http://0.0.0.0:4723/wd/hub")
+end
