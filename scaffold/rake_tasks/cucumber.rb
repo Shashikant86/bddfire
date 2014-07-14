6ab546beb05ce3aa4eaf54115bc976a8284c9194
Shashikant <shashikant.jagtap@aol.co.uk>
2014-07-14 15:16:40 +0100

    Added CI support

--- scaffold/rake_tasks/cucumber.rb
+++ scaffold/rake_tasks/cucumber.rb
@@ -2,5 +2,7 @@ require 'cucumber/rake/task'
 Cucumber::Rake::Task.new(:features) do |features|
   features.cucumber_opts = "features -p selenium --format progress"
 end
-
+Cucumber::Rake::Task.new(:features_ci) do |task|
+  task.cucumber_opts = ["-p poltergeist -f pretty -f junit --out target/ -f html --out target/report.html"]
+end
 
