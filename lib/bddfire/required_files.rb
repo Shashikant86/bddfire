6ee641ca44bc9da8189badcfdeb09d43bb132768
Shashikant86 <shashikant.jagtap@aol.co.uk>
2014-07-17 21:01:34 +0100

    Added test steps

---
+++ lib/bddfire/required_files.rb
@@ -0,0 +1,9 @@
+require 'rubygems'
+require 'bundler/setup'
+require "Capybara/cucumber"
+require "rspec"
+require 'capybara/poltergeist'
+require 'selenium-webdriver'
+require 'rubocop'
+require 'rbconfig'
+include RbConfig
---
+++ lib/bddfire/ui_steps.rb
@@ -0,0 +1,11 @@
+require_relative 'required_files'
+
+Given(/^I am on the "(.*?)"$/) do |url|
+  session = Capybara::Session.new(:poltergeist)
+  session.visit(url)
+end
+
+Then(/^I should see the text "(.*?)"$/) do |text|
+  session = Capybara::Session.new(:poltergeist)
+  session.page.should have_content(text)
+end
