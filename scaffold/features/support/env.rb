25f8431d00fc69d667703621536e1465260c2ab7
Shashikant <shashikant.jagtap@aol.co.uk>
2014-07-23 11:16:17 +0100

    Selenium Driver registration

--- scaffold/features/support/env.rb
+++ scaffold/features/support/env.rb
@@ -7,8 +7,21 @@ require 'selenium-webdriver'
 require 'rubocop'
 require 'bddfire'
 
+Capybara.configure do |config|
 
-Capybara.default_driver = :selenium
+  config.run_server = false
+  config.default_driver = (ENV['DRIVER'] || 'selenium').to_sym
+  config.javascript_driver = config.default_driver
+  config.default_selector = :xpath
+  config.default_wait_time = 60
+end
+
+
+Capybara.register_driver :selenium do |app|
+
+  profile = Selenium::WebDriver::Firefox::Profile.new
+  Capybara::Selenium::Driver.new(app, :profile => profile)
+end
 
 Capybara.register_driver :poltergeist do |app|
 
