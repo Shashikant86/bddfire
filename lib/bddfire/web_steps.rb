5f4cae83bd32ef5e56e0aef8d33236b75ce0d047
Shashikant86 <shashikant.jagtap@aol.co.uk>
2015-04-04 01:13:00 +0100

    Adding web steps

--- lib/bddfire/web_steps.rb
+++ lib/bddfire/web_steps.rb
@@ -1,13 +1,13 @@
-# require_relative 'web/web_methods'
-#
-# Given(/^I am on "(.*?)"$/) do |url|
-#   visit(url)
-# end
-#
-# When(/^I fill in "(.*?)" with "(.*?)"$/) do |element, text|
-#   fill_in element, with: text
-# end
-#
-# Then(/^I should see "(.*?)"$/) do |text|
-#   page.should have_content text
-# end
+require_relative 'web/web_methods'
+
+Given(/^I am on "(.*?)"$/) do |url|
+  visit(url)
+end
+
+When(/^I fill in "(.*?)" with "(.*?)"$/) do |element, text|
+  fill_in element, with: text
+end
+
+Then(/^I should see "(.*?)"$/) do |text|
+  page.should have_content text
+end
--- scaffold/features/google.feature
+++ scaffold/features/google.feature
@@ -2,6 +2,6 @@ Feature: Google Search to explore BDDfire
 
 
 Scenario: View home page
-  Given I am on "www.google.com"
+  Given I am on "http://www.google.com"
   When I fill in "q" with "shashi"
   Then I should see "Sign in"
