1e278f0b359f277fd063facb114bd9807ce88616
Shashikant86 <shashikant.jagtap@aol.co.uk>
2015-04-06 21:39:33 +0100

    Added headless steps for Poltergeist

---
+++ lib/bddfire/headless_steps.rb
@@ -0,0 +1,65 @@
+require_relative 'web/web_methods'
+
+Then(/^the response status code should be "(.*?)"$/) do |code|
+  page_code = page.driver.status_code
+  expect(page_code).to eq(code)
+end
+
+Then(/^response headers should contain "(.*?)"$/) do |expected_header|
+  header = page.response_headers
+  expect(header).to include(expected_header)
+end
+
+Then(/^page source should contain "(.*?)"$/) do |data|
+   source = page.driver.source
+   expect(source).to include(data)
+end
+
+When(/^I clear network traffic$/) do
+   page.driver.clear_network_traffic
+end
+
+When(/^I called network traffic of the page$/) do
+  page.driver.network_traffic.each do |request|
+  request.response_parts.uniq(&:url).each do |response|
+    puts "\n Responce URL #{response.url}: \n Status #{response.status}"
+  end
+end
+end
+
+Then(/^network traffic should contains resource "(.*?)"$/) do |resource|
+  traffic = page.driver.network_traffic
+  expect(traffic).to include(resource)
+end
+
+When(/^I request response headers$/) do
+  puts page.response_headers.to_a
+end
+
+When(/^I request page "(.*?)" with header name "(.*?)" value "(.*?)"$/) do |url, name, value|
+  page.driver.headers = { '#{name}' => '#{value}' }
+  visit(url)
+end
+
+When(/^I set the cookie name "(.*?)" with value "(.*?)"$/) do |name, value|
+  page.driver.set_cookie(name, value)
+end
+
+
+Then(/^I should see the cookie "(.*?)"$/) do |cookie|
+  cookies = page.driver.cookies
+  expect(cookies).to include(cookie)
+end
+
+When(/^I removed cookie "(.*?)"$/) do |arg1|
+  page.driver.remove_cookie
+end
+
+When(/^I clear all cookies$/) do
+  page.driver.clear_cookies
+end
+
+When(/^I visit page with base authorization user "(.*?)" and password "(.*?)"$/) do |user, password|
+  #page.driver.headers = {'Authorization': 'Basic '+ Base64.encode64('username:password')};
+  page.driver.basic_authorize(user, password)
+end
---
+++ pre-defined-steps/headless_steps.md
@@ -0,0 +1,18 @@
+# Headless Steps
+
+Noe: these steps will only work with Poltergeist driver for Capybara. Don't use it with Selenium
+
+
+         Then the response status code should be "200"
+         Then response headers should contain ""
+         Then page source should contain ""
+         When I clear network traffic
+         When I called network traffic of the page
+         Then network traffic should contains resource ""
+         When I request response headers
+         When I request page "url" with header name "dd" value ""
+         When I set the cookie name "cookie" with value "ss"
+         Then I should see the cookie ""
+         When I removed cookie "cookie"
+         When I clear all cookies
+         When I visit page with base authorization user "" and password ""
